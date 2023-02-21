0a019210c17ef50aa352fec44b87025787c42689
pipixia <741804121@qq.com>
2023-02-21 22:31:58 +0800

    1

--- Unity/Assets/Scripts/Codes/Model/Share/GameConfig.cs
+++ Unity/Assets/Scripts/Codes/Model/Share/GameConfig.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+
 
 namespace ET
 {
@@ -95,5 +92,17 @@ namespace ET
 
     }
 
+    public static class HttpConfig
+    {
+        public const int PostMaxDataLength = 4096;
+        public const string Exp = "exp";
+        public const string UUID = "uuid";
+        public const string Key = "key";
+        public const string TokenPrefix = "Bearer ";
+        public const string Authorization = "Authorization";
+        public const string AuthSecret = "wIkOFc3QwBmwwefDKPgxeIele7JuQygD";
+        public const string CustomAuthorization = "CustomAuthorization";
+        public const string DotaDedicatedServerKeyV2 = "wIkOFc3QwBmwwefDKPgxeIele7JuQygD";
 
+    }
 }
